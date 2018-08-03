58aa34811078afab61e9c24143fa3e393dd459c4
lafant <anwar9609@yahoo.com>
2018-08-03 18:16:26 +0800

    Delete bmon.sh

--- bmon.sh
+++
@@ -1,7 +0,0 @@
-git clone https://github.com/tgraf/bmon.git
-cd bmon
-yum install make libconfuse-devel libnl3-devel libnl-route3-devel ncurses-devel -y
-./autogen.sh
-./configure
-make
-make install
