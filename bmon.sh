4960d826687d632d416a62f31f278965db69f08c
lafant <anwar9609@yahoo.com>
2018-08-03 18:19:44 +0800

    Update bmon.sh

--- bmon.sh
+++ bmon.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/tgraf/bmon.git
 cd bmon
-yum install make libconfuse-devel libnl3-devel libnl-route3-devel ncurses-devel
+yum install make libconfuse-devel libnl3-devel libnl-route3-devel ncurses-devel -y
 ./autogen.sh
 ./configure
 make
