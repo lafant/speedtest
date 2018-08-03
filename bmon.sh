6485cdff77f559e8ff76f0b6359f73497b64fcf6
lafant <anwar9609@yahoo.com>
2018-08-03 18:16:49 +0800

    Add files via upload

---
+++ bmon.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/tgraf/bmon.git
+cd bmon
+yum install make libconfuse-devel libnl3-devel libnl-route3-devel ncurses-devel
+./autogen.sh
+./configure
+make
+make install
+bmon
